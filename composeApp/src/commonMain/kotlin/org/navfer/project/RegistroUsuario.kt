7e41f88c0ea10e28fe48062b0d2394d8573efc2a
Natalia <navfernandez03@gmail.com>
2025-03-05 12:36:28 +0100

    se puede agregar avatar en registro de usuario

--- composeApp/src/commonMain/kotlin/org/navfer/project/RegistroUsuario.kt
+++ composeApp/src/commonMain/kotlin/org/navfer/project/RegistroUsuario.kt
@@ -1,5 +1,7 @@
 package org.navfer.project
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Visibility
 import androidx.compose.material.icons.filled.VisibilityOff
@@ -11,17 +13,43 @@ import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.painter.BitmapPainter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.loadImageBitmap
+import androidx.compose.ui.window.AwtWindow
 import androidx.navigation.NavHostController
+import java.awt.FileDialog
+import java.awt.Frame
+import java.io.File
+import java.io.FilenameFilter
+import java.util.Base64
 
 @Composable
 fun RegistroUsuario(navController: NavHostController, modifier: Modifier = Modifier,registerFunction:((String,String,String) -> Boolean)? = null){
     var usuario by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
-    var avatar by remember{ mutableStateOf("") }
+    var avatarRuta by remember { mutableStateOf("") }
+    var avatarBase64 by remember { mutableStateOf<String?>(null) }
     var isPasswordVisible by remember { mutableStateOf(false) }
-    var enabledButton =usuario.isNotBlank() && password.isNotBlank()
+    var isImageDialogOpen by remember { mutableStateOf(false) }
+    var enabledButton = usuario.isNotBlank() && password.isNotBlank()
     var showError by remember { mutableStateOf(false) }
 
+
+    fun convertirBase64(imageFile: File): String {
+        val bytes = imageFile.readBytes()
+        return Base64.getEncoder().encodeToString(bytes)
+    }
+    if (isImageDialogOpen) {
+        AvatarChooserRegistro { selectedFile ->
+            isImageDialogOpen = false
+            if (selectedFile != null) {
+                avatarRuta = selectedFile.absolutePath
+                avatarBase64 = convertirBase64(selectedFile)
+            }
+        }
+    }
     Box(
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
@@ -31,52 +59,66 @@ fun RegistroUsuario(navController: NavHostController, modifier: Modifier = Modif
             verticalArrangement = Arrangement.Center,
         ) {
             Text(
-                text = "Registro",
+                text = "N U E V O  U S U A R I O",
                 style = androidx.compose.material3.MaterialTheme.typography.headlineMedium,
                 modifier = Modifier.padding(bottom = 16.dp)
             )
-            // Campo de usuario
-            OutlinedTextField(
-                value = usuario,
-                onValueChange = { usuario = it },
-                label = { Text("Usuario") },
-                placeholder = { Text("Introduce  usuario") },
-                singleLine = true,
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-            // Campo de contraseña
-            OutlinedTextField(
-                value = password,
-                onValueChange = { password = it },
-                label = { Text("Contraseña") },
-                placeholder = { Text("Introduce contraseña") },
-                singleLine = true,
-                visualTransformation = if (isPasswordVisible) VisualTransformation.None else PasswordVisualTransformation(),
-                trailingIcon = {
-                    IconButton(onClick = { isPasswordVisible = !isPasswordVisible }) {
-                        val icon = if (isPasswordVisible) Icons.Default.Visibility else Icons.Default.VisibilityOff
-                        Icon(icon, contentDescription = if (isPasswordVisible) "Ocultar contraseña" else "Mostrar contraseña")
-                    }
-                },
-            )
-            OutlinedTextField(
-                value = avatar,
-                onValueChange = { avatar = it },
-                label = { Text("Avatar") },
-                placeholder = { Text("Introduce la ruta de tu avatar") },
-                singleLine = true,
-            )
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ){
+                Column {
+                    // Campo de usuario
+                    OutlinedTextField(
+                        value = usuario,
+                        onValueChange = { usuario = it },
+                        label = { Text("Usuario") },
+                        placeholder = { Text("Introduce  usuario") },
+                        singleLine = true,
+                    )
+                    Spacer(modifier = Modifier.height(8.dp))
+                    // Campo de contraseña
+                    OutlinedTextField(
+                        value = password,
+                        onValueChange = { password = it },
+                        label = { Text("Contraseña") },
+                        placeholder = { Text("Introduce contraseña") },
+                        singleLine = true,
+                        visualTransformation = if (isPasswordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+                        trailingIcon = {
+                            IconButton(onClick = { isPasswordVisible = !isPasswordVisible }) {
+                                val icon = if (isPasswordVisible) Icons.Default.Visibility else Icons.Default.VisibilityOff
+                                Icon(icon, contentDescription = if (isPasswordVisible) "Ocultar contraseña" else "Mostrar contraseña")
+                            }
+                        },
+                    )
+                }
+
+                //vista previa del avatar
+                if (avatarRuta.isNotEmpty()) {
+                    //Spacer(modifier = Modifier.height(8.dp))
+                    MyAvatar(
+                        ruta = avatarRuta,
+                        modifier = Modifier.size(150.dp)
+                    )
+                }
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+            Button(onClick = { isImageDialogOpen = true }) {
+                Text("Seleccionar Avatar")
+            }
 
             Spacer(modifier = Modifier.height(16.dp))
 
-            Row(modifier = Modifier.padding(2.dp)){
+            Row(modifier = Modifier.padding(2.dp)) {
                 Button(
                     onClick = {
-                        if (avatar.isBlank()){
-                            avatar = ""
-                        }
                         if (registerFunction != null) {
-                            val success = registerFunction(usuario, password ,avatar)
+                            val success = registerFunction(usuario, password,
+                                avatarBase64.toString()
+                            )
                             if (success) {
                                 navController.navigate("login")
                             } else {
@@ -85,11 +127,9 @@ fun RegistroUsuario(navController: NavHostController, modifier: Modifier = Modif
                         }
                     },
                     enabled = enabledButton
-
                 ) {
                     Text("Registrarse")
                 }
-
             }
             Spacer(modifier = Modifier.height(16.dp))
 
@@ -105,4 +145,43 @@ fun RegistroUsuario(navController: NavHostController, modifier: Modifier = Modif
 
         }
     }
+}
+
+@Composable
+fun MyAvatar(ruta: String,modifier: Modifier = Modifier) {
+    val file = File(ruta)
+    val imageBitmap = file.inputStream().buffered().use(::loadImageBitmap)
+
+    Image(
+        painter = BitmapPainter(imageBitmap),
+        contentDescription = "Avatar",
+        modifier = modifier
+            .size(150.dp)
+            .clip(CircleShape),
+        contentScale = ContentScale.Crop
+    )
+}
+
+@Composable
+fun AvatarChooserRegistro(onClose: (File?) -> Unit) {
+    AwtWindow(
+        create = {
+            object : FileDialog(null as Frame?, "Selecciona una imagen JPEG", FileDialog.LOAD) {
+                init {
+                    directory = System.getProperty("user.home")
+                    this.filenameFilter = FilenameFilter { _, name ->
+                        name.endsWith(".jpg", ignoreCase = true) || name.endsWith(".jpeg", ignoreCase = true)
+                    }
+                }
+
+                override fun setVisible(value: Boolean) {
+                    super.setVisible(value)
+                    if (value) {
+                        onClose(file?.let { File(directory, it) })
+                    }
+                }
+            }
+        },
+        dispose = FileDialog::dispose
+    )
 }
